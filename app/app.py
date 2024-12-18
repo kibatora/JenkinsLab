c153df7ecb45a9e5e0b5ea1f1e1995bf1dd5dc85
Ivan <zaranet1999@gmail.com>
2024-12-18 18:16:58 +0700

    izmen

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Jeirld!"
+    return "Hello, world of Jeirld!"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
